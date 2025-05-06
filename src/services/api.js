155e5aa80714bd37997ab2f0e0ee8840fa148408
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-05-06 18:50:11 +0200

    update config

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,43 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = '';
-
+const API_KEY = 'f66f1892c1c72d0d87a3d99d5984fa7e'; 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  headers: {
-    Authorization: API_KEY,
-  },
 });
 
 export const fetchTrendingMovies = () => {
-  return axiosInstance.get('/trending/movie/day').then(res => res.data.results);
+  return axiosInstance.get('/trending/movie/day', {
+    params: { api_key: API_KEY }, 
+  }).then(res => res.data.results);
 };
 
 export const searchMovies = query => {
-  return axiosInstance.get(`/search/movie?query=${query}&include_adult=false`).then(res => res.data.results);
+  return axiosInstance.get(`/search/movie`, {
+    params: {
+      api_key: API_KEY, 
+      query: query,
+      include_adult: false,
+    },
+  }).then(res => res.data.results);
 };
 
 export const fetchMovieDetails = id => {
-  return axiosInstance.get(`/movie/${id}`).then(res => res.data);
+  return axiosInstance.get(`/movie/${id}`, {
+    params: { api_key: API_KEY }, 
+  }).then(res => res.data);
 };
 
 export const fetchMovieCredits = id => {
-  return axiosInstance.get(`/movie/${id}/credits`).then(res => res.data.cast);
+  return axiosInstance.get(`/movie/${id}/credits`, {
+    params: { api_key: API_KEY },
+  }).then(res => res.data.cast);
 };
 
 export const fetchMovieReviews = id => {
-  return axiosInstance.get(`/movie/${id}/reviews`).then(res => res.data.results);
+  return axiosInstance.get(`/movie/${id}/reviews`, {
+    params: { api_key: API_KEY },
+  }).then(res => res.data.results);
 };
 
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
